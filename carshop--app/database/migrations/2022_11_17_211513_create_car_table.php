b770ba526c7433cda393bdbca9f21f8f25ffc196
unknown <faustin.garreau@gmail.com>
2022-11-17 22:35:18 +0100

    first commit, controller and model was created

---
+++ carshop--app/database/migrations/2022_11_17_211513_create_car_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car', function (Blueprint $table) {
+            $table->id();
+            $table->boolval('gearbox');
+            $table->string('immat');
+            $table->string('color');
+            $table->integer('door_number');
+            $table->decimal('kilometers');
+            $table->integer('nb_places');
+            $table->boolean('clim');
+            $table->string('oil');
+            $table->string('categories');
+            $table->text('image');
+            $table->text('licence');
+            $table->decimal('licence');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car');
+    }
+};
